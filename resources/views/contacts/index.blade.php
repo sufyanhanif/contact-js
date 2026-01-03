5018487707dc647dcc2719cbadd40ca732b3823b
sufyanhanif <hanifsufyan48@gmail.com>
2026-01-03 16:37:55 +0700

    center mid

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -10,11 +10,26 @@
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <ul>
-        @foreach($contacts as $contact)
-            <li>{{ $contact->name }} - {{ $contact->email }} - {{ $contact->phone }}</li>
-        @endforeach
-    </ul>
-    
+    <div class="flex justify-center">
+    <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 mt-6  ">
+    @foreach($contacts as $contact)
+        <li class="bg-white rounded-xl shadow-md p-5 hover:shadow-lg transition">
+            <h3 class="text-lg font-semibold text-gray-800">
+                {{ $contact->name }}
+            </h3>
+
+            <p class="text-sm text-gray-600 mt-1">
+                📧 {{ $contact->email }}
+            </p>
+
+            <p class="text-sm text-gray-600">
+                📞 {{ $contact->phone }}
+            </p>
+        </li>
+    @endforeach
+</ul>
+</div>
+   
+
 </body>
 </html>
